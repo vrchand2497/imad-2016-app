5f51754bd0d915567c65b1b08e44ffa6b0655313
vrchand2497 <v.r.chand97@gmail.com>
2016-10-09 23:23:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/images(7).jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'images(7).jpg'));
+app.get('/ui/images(7).jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'images(7).jpeg'));
 });
 
 
