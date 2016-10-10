7ca2623a470bcf196957c9893da30c95a8efaac9
vrchand2497 <v.r.chand97@gmail.com>
2016-10-10 21:56:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/a1', function (req, res) {
+app.get('/abs', function (req, res) {
   res.sendFile(path.join('this is first article'));
 });
 app.get('/a2', function (req, res) {
