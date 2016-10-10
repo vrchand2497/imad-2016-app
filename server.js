002c1a056c9127627c997bb47def6b0194bd2582
vrchand2497 <v.r.chand97@gmail.com>
2016-10-10 11:31:52 +0530

    jdj

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
     
-app.get('/ui/nick.png', function (req, res) {
+app.get('/ui/power.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'power.jpg'));
 
 });
